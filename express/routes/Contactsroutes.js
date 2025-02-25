39e15bb884cc59140cacfc6b0580f32cb30735a5
a4dhhh <adarshmohanann@gmail.com>
2025-02-25 15:49:57 +0530

    UPDATE

--- express/routes/Contactsroutes.js
+++ express/routes/Contactsroutes.js
@@ -1,3 +1,33 @@
 const express = require('express')
 const router = express.Router()
 const Contacts = require('../models/ContactModel')
+const Contacts = require('../models/ContactModel')
+
+
+router.get('/all', async (req,res) =>{
+    try {
+  const Contacts = await Contacts.find()
+  return res.status(200).json(Contacts)
+    } catch (error) {
+        return res.status(500).json({message: error.message })
+    }
+})
+
+
+
+router.post('/add', async (req,res) => {
+    try{
+        const newcontact = new Contacts(req.body)
+        const { name,phone } = newcontact
+        if (!phone || !name ) {
+            return res.status(400).json({message: "All fields required"})
+        }
+        await newcontact.save()
+        return res.status(200).json(newcontact)
+    }
+    catch (error) {
+            return res.status(500).json({message: error.message })
+        }
+})
+
+module.exports = router
